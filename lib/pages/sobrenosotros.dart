d0e33922029348ef96e2cdd1e0a0e6b8d7413f7a
merariGrande <idalia.grande22@itca.edu.sv>
2023-10-05 10:55:36 -0600

    cambiando sobre nosotros

--- lib/pages/sobrenosotros.dart
+++ lib/pages/sobrenosotros.dart
@@ -5,7 +5,7 @@ class SobreNosotrosScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Pagina Sobre nosotros'),
+        title: Text('Sobre nosotros'),
       ),
       body: Center(
         child: Column(

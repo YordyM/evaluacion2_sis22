d9219e4ae967f7cd202e385a4d70ce6ae290661d
rnloopzz <rl444705@gmail.com>
2023-10-05 20:55:09 -0600

    probando

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'MY APP YORDY, IDALIA, RENÉ'),
+      home: const MyHomePage(title: 'MY APP YORDY, IDALIA, RENÉ 1111'),
     );
   }
 }

e6a581b1e640915b428f5661d6de48295b837142
YordyM <yordi.romero22@itca.edu.sv>
2023-10-05 21:17:09 -0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,6 @@ import 'package:eval_sis22/pages/home.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp().then((_) {
-    runApp(const MyApp());
-  });
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -17,12 +10,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Evaluacion2 sis22',
+      title: 'titulo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'MY APP YORDY, IDALIA, RENÉ 1111'),
+      home: const MyHomePage(title: 'MY APP '),
     );
   }
 }
